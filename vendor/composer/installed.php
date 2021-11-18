6f54cb7ffb47d17bc666d163579cb90dfc2b59c4
MatheusOlivr <contato.matheusoliv@gmail.com>
2021-11-18 00:00:49 -0300

    Instala e configura o composer e move a pasta public
    O arquivo composer.json do projeto foi configurado e a pasta public
    foi movida para a raiz do projeto, pois estava no diretório errado.

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '82b86f21f31529f1a2d2860ae39f83c922f942b4',
+        'name' => 'matheusolivr/miniframework',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'matheusolivr/miniframework' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '82b86f21f31529f1a2d2860ae39f83c922f942b4',
+            'dev_requirement' => false,
+        ),
+    ),
+);
