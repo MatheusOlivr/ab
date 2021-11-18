edb067ba1ddec35a57c5ba8d3adc2565d2ec0ed2
MatheusOlivr <contato.matheusoliv@gmail.com>
2021-11-18 09:38:51 -0300

    Configura o arquivo route.php para acessar as rotas do nosso framework
    Ele agora acessa o  arquvio indexController.php que está na pasta
    Controllers e executa o método correspondente aquela rota

--- App/Route.php
+++ App/Route.php
@@ -2,6 +2,20 @@
 	namespace App;
 	class Route
 	{
+		private $routes;
+		public function __construct()
+		{
+			$this->initRoutes();
+			$this->run($this->getUrl());
+		}
+		public function getRoutes()
+		{
+			return $this->routes;
+		}
+		public function setRoutes($routes)
+		{
+			$this->routes = $routes;
+		}
 		public function initRoutes()
 		{
 			$routes["home"] = array(
@@ -12,8 +26,22 @@
 			$routes["sobre_nos"] = array(
 				"route" => "/sobre_nos",
 				"controller" => "indexController",
-				"action" => "sobre_nos"
+				"action" => "sobreNos"
 			);
+			$this->setRoutes($routes);
+		}
+		public function run($url)
+		{
+			foreach($this->getRoutes() as $path => $route)
+			{
+				if($url == $route["route"])
+				{
+					$class = "App\\Controllers\\".ucfirst($route["controller"]);
+					$controller = new $class;
+					$action = $route["action"];
+					$controller->$action();
+				}
+			}
 		}
 		public function getUrl()
 		{
