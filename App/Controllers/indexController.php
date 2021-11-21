5fe7ce27bf59e715974a93736e66d805ce519e4b
MatheusOlivr <contato.matheusoliv@gmail.com>
2021-11-21 01:04:15 -0300

    Cria conexao com o banco e retorna os dados da tabela do banco diretamente na view

--- App/Controllers/indexController.php
+++ App/Controllers/indexController.php
@@ -1,16 +1,30 @@
 <?php 
 	namespace App\Controllers;
 	use MF\Controller\Action;
+	use App\Connection;
+	use App\Models\Product;
 	class IndexController extends Action
 	{
 		public function index()
 		{
-			$this->view->data = array("sofa","cadeira","cama");
+			//$this->view->data = array("sofa","cadeira","cama");
+
+			// instacia da conexao
+			$conn = Connection::getDb();
+			//instânciar modelo
+			$product = new Product($conn);
+			$products = $product->getProducts();
+			$this->view->data = $products;
 			$this->render("index","layoutDefault");
 		}
 		public function sobreNos()
 		{
-			$this->view->data = array("notebook","smartphone");
+			//$this->view->data = array("notebook","smartphone");
+			$conn = Connection::getDb();
+			//instânciar modelo
+			$product = new Product($conn);
+			$products = $product->getProducts();
+			$this->view->data = $products;
 			$this->render("sobre_nos","layoutDefault");	
 		}
 	}
